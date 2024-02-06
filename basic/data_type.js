dc514cb44ad8f2d76cbddb1212753654ab934f3d
sanjeetgit1 <115420418+sanjeetgit1@users.noreply.github.com>
2024-02-06 17:24:45 +0000

    data_type

---
+++ basic/data_type.js
@@ -0,0 +1,19 @@
+let Number =39
+let islogedIn=false;
+let state;//undefined
+let age=20
+let Number1=null //null means kuchh nahi hai khali hai
+
+// BigInt
+// String
+// Boolean;
+// null
+// undefined
+// Symbol=> unique
+
+console.log(Number)
+
+console.log(typeof age)
+console.log(typeof 'sanjeet')
+console.log(typeof null)// NULL IS a object 
+console.log(typeof undefined) // special type is undefined
