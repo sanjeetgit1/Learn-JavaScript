f133f8ebb7faf8b1da8ec5db5da62331983dae6e
sanjeetgit1 <115420418+sanjeetgit1@users.noreply.github.com>
2024-02-07 05:57:09 +0000

    operation

--- basic/conversionOperation.js
+++ basic/conversionOperation.js
@@ -5,18 +5,17 @@ let score ="33"
 
 let score1 =false;
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)cl
  
 let valueIntNumber = Number(score)
-console.log( typeof valueIntNumber)
+// console.log( typeof valueIntNumber)
 
-console.log(typeof score1)
+// console.log(typeof score1)
 
 let boolean1="sanjeet"
 let changeboolean =Boolean(boolean1)
 
-console.log(changeboolean)
+// console.log(changeboolean)
 
 //  1=>true  , 0=>false
 // ""=>false 
@@ -25,5 +24,33 @@ console.log(changeboolean)
 let oneNumber = 33
 
 let changeString =String(oneNumber)
-console.log(changeString)
-console.log(typeof (changeString))
+// console.log(changeString)
+// console.log(typeof (changeString))
+
+
+
+// ====================operation================================================
+
+let value = 3
+let negvalue =-value
+// console.log(negvalue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(3%2)
+
+console.log("1"+3)
+
+console.log(1+"2"+4)
+console.log(1+"2")
+console.log(1+3+"2")
+console.log("1"+2+2)
+
+console.log(+true)
+console.log(+"")
+
+let num;
+num=num1=num2=5
+console.log(num2)
