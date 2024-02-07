32407d0afa3b6ab0230a37faad8de227341bb180
sanjeetgit1 <115420418+sanjeetgit1@users.noreply.github.com>
2024-02-07 05:26:08 +0000

    conversion of datatype

---
+++ basic/conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = 33
+
+
+let score ="33"
+
+let score1 =false;
+
+console.log(typeof score)
+console.log(typeof (score))
+ 
+let valueIntNumber = Number(score)
+console.log( typeof valueIntNumber)
+
+console.log(typeof score1)
+
+let boolean1="sanjeet"
+let changeboolean =Boolean(boolean1)
+
+console.log(changeboolean)
+
+//  1=>true  , 0=>false
+// ""=>false 
+//"sanjeet"=>true
+
+let oneNumber = 33
+
+let changeString =String(oneNumber)
+console.log(changeString)
+console.log(typeof (changeString))
