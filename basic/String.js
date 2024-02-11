9f6bfc23f86a724e6b9f81b8c7ff10b6870874e1
sanjeet kumar <115420418+sanjeetgit1@users.noreply.github.com>
2024-02-11 10:48:25 +0000

    basic string

---
+++ basic/String.js
@@ -0,0 +1,21 @@
+const name = 'sanjeet'
+const ropocount =50
+console.log(name)
+console.log(ropocount)
+
+console.log(`My name is ${name} and i have a ${ropocount} rupeya`)//modern way wirte string  whose say \\
+
+const game = new String('sanjeet')
+
+console.log(game.toLowerCase())
+console.log(game.toUpperCase())
+console.log(game.charAt(3))
+console.log(game.indexOf('j'))
+
+const newstring = game.substring(1,4)
+console.log(newstring)
+
+const num =game.slice(0,4)
+console.log(num)
+
+// const number =game.split(2);
